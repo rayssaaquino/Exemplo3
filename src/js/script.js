71767906163472253bf4c0d66f9d924ec2463b1c
Rayssa <rayssaaquino77@gmail.com>
2025-04-04 11:03:05 -0300

    Adicionando Metodos

--- src/js/script.js
+++ src/js/script.js
@@ -36,3 +36,42 @@ console.log(typeof ex4)
 let ex5=[];
 console.log(typeof ex5)
 
+//CONVERSÕES
+
+//FLOAT => STRING
+
+let numFloat =123.456;
+console.log(numFloat.toString())
+
+//STRING => FLOAT
+
+let numString ="12.963";
+console.log(parseFloat(numString))
+
+//INT => STRING
+
+let numInt ="100";
+console.log (numInt.toString)
+
+// STRING =>
+
+//METODOS
+//METODOS LENGTH - VERIFICA O TAMANHO DA string
+
+let numString1 ="120";
+console.log (parseInt(numString))
+
+let frase = "O mundo da tecnologia";
+console.log (frase.length)
+
+//METODO INDEOF/lastIndexOf- RETORNA UM TRECHO DO SEU CODIGO
+
+let texto ="Programação Sustentável;"
+console.log (texto.indexOf("ão"))
+
+//METODO SLICE - RETORNA PARTE DO TEXTO PASSANDO INICIO E O FINAL
+
+let info="Progamação de ponta";
+console.log (info.slice(14,20))
+
+//OPRADORES ARITMETICOS
