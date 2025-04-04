9bd11f0ed8fccf2a2303ed23b22b945821e4ef78
Rayssa <rayssaaquino77@gmail.com>
2025-04-04 10:22:37 -0300

    criando projeto exemplo3

---
+++ src/js/script.js
@@ -0,0 +1,38 @@
+// DECLARAÇÃO E VARIAVEIS
+
+var nome ="Fiap";
+console.log(nome)
+
+let idade =19;
+console.log(idade)
+
+const sobrenome ="Fiapinho";
+console.log(sobrenome)
+
+//undefined
+
+let exemplo;
+console.log(exemplo)
+
+//nula
+
+let exemplo1 =null;
+console.log(exemplo1)
+
+//TIPOS DE VARIAVEIS
+
+let ex1 ="Fiap";
+console.log(typeof ex1)
+
+let ex2 =25;
+console.log(typeof ex2)
+
+let ex3 =true;
+console.log(typeof ex3)
+
+let ex4={};
+console.log(typeof ex4)
+
+let ex5=[];
+console.log(typeof ex5)
+
